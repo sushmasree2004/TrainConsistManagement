6ec2376e68cb02fd798e23662c52be459a2035d1
sushmasree2004 <sushmasree2004@gmail.com>
2026-03-10 12:07:18 +0530

    Intialize Train Management App

---
+++ TrainIntialize.java
@@ -0,0 +1,37 @@
+package com.Train;
+
+
+/*
+ * Description:  
+ * 
+ * This class represents the entry point of the 
+ * Train Consist Management Application.
+ * 
+ * At this stage, the application:
+ * Creates an empty train consist
+ * Uses a dynamic List to store bogies
+ * Displays initial bogie count
+ * Prints the current state of the train
+
+
+@Author B.Sushma Sree
+@Version 1.0
+*/
+import java.util.*;
+public class Main 
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("Train Consist Management APP ");
+		
+		List<String> trainConsist=new ArrayList<>();
+		
+		System.out.print("Train initialized Successfully...");
+		
+		
+		
+		
+	}
+
+}
